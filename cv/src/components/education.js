97085dce7228164c166895dd093304f858f6bdf0
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 18:13:16 +0530

    added header

--- cv/src/components/education.js
+++ cv/src/components/education.js
@@ -3,34 +3,46 @@ import {Container, Table} from 'react-bootstrap'
 
 function education() {
     return (
-        <Container>
-            <Table responsive bordered hover size="sm">
-                <thead style={{backgroundColor:"darkblue", color:"white", textDecoration:"bold"}}>
+        <Container className="mt-3">
+            <Table responsive bordered size="sm">
+
+                <thead>
+                    <th colSpan="4">
+                        Education
+                    </th>
+                </thead>
+
+                <thead>
+
                     <th>Programme</th>
                     <th>Institution</th>
                     <th>% / CGPA</th>
                     <th>Year</th>
+
                 </thead>
 
                 <tbody>
-                <tr>
-                    <td>B.Tech</td>
-                    <td>NIT Meghalaya</td>
-                    <td>8.01</td>
-                    <td>2016-2020</td>
-                </tr>
-                <tr>
-                    <td>XII (CBSE)</td>
-                    <td>BSF, Senior Seconday School, Shillong</td>
-                    <td>80.00</td>
-                    <td>2016</td>
-                </tr>
-                <tr>
-                    <td>X (ICSE)</td>
-                    <td>St. Edmund's School, Shillong</td>
-                    <td>89.90</td>
-                    <td>2014</td>
-                </tr>
+
+                    <tr>
+                        <td>B.Tech</td>
+                        <td>NIT Meghalaya</td>
+                        <td>8.01</td>
+                        <td>2016-2020</td>
+                    </tr>
+
+                    <tr>
+                        <td>XII (CBSE)</td>
+                        <td>BSF, Senior Seconday School, Shillong</td>
+                        <td>80.00</td>
+                        <td>2016</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>X (ICSE)</td>
+                        <td>St. Edmund's School, Shillong</td>
+                        <td>89.90</td>
+                        <td>2014</td>
+                    </tr>
 
                 </tbody>
 
