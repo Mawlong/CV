154cdcf0ce142ce469c11d55142c1c07f83c7248
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 17:30:02 +0530

    added navbar

--- cv/src/components/navbar.js
+++ cv/src/components/navbar.js
@@ -4,7 +4,7 @@ import {Navbar} from 'react-bootstrap'
 function navbar() {
     return (
         
-        <Navbar bg='dark' className="justify-content-center mx-auto navbar-dark" >
+        <Navbar className="justify-content-center mx-auto navbar-dark px-2" style={{backgroundColor:"#2680d9"}}>
             <Navbar.Brand href="/">Leon Patrick Mawlong CV</Navbar.Brand>
         </Navbar>
             
