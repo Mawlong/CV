d41793954b2d0355a80090841f584a82dbc7defc
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 18:13:23 +0530

    added techprof

---
+++ cv/src/components/technicalProficiency.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Container, Table} from 'react-bootstrap'
+
+function technicalProficiency() {
+    return (
+        <Container>
+            <Table responsive bordered size="sm"> 
+                <thead>
+                    <th>
+                        Languages, frameworks, Software and Libraries
+                    </th>
+                </thead>
+                <tbody>
+                    <td>C, C++, Python, Arduino, git, HTML, CSS, Java Script, Adobe Photoshop, Adobe Premiere Pro, Adobe Lightroom, Adobe Xd</td>
+                </tbody>
+            </Table>
+            
+        </Container>
+    )
+}
+
+export default technicalProficiency
