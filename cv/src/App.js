2a968ccca6d55c2dd1432bbfb0ca4f16aaacbb62
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 18:13:30 +0530

    included tect prof

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
 import Education from './components/education'
+import Proficiency from './components/technicalProficiency'
 
 
 import Navigation from './components/navbar';
 import Footer from './components/footer';
-
-
-
-// export async function getServerSideProps() {
-
-//   const getEducation
-// }
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Education/>
+      <Proficiency/>
       <Footer/>
     </div>
 
