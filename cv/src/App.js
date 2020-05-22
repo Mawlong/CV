149dd1f4eba6cb83bb5a91ae953158ec19a85f79
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 14:33:16 +0530

    removed unecessary files and set up initial code base

---
+++ cv/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      Hellow thereeeeee
+     
+    </div>
+  );
+}
+
+export default App;
