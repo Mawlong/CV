8bf0a9139e7c2fbefb781816c57d2d6023f16725
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 14:39:48 +0530

    added navbar

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navigation from './components/navbar';
+
 function App() {
   return (
     <div className="App">
-
-      Hellow thereeeeee
-     
+      <Navigation/>
     </div>
   );
 }
---
+++ cv/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+
+function navbar() {
+    return (
+        
+        <Navbar bg='dark' className="justify-content-center mx-auto navbar-dark" >
+            <Navbar.Brand href="/">Leon Patrick Mawlong CV</Navbar.Brand>
+        </Navbar>
+            
+        
+    )
+}
+
+export default navbar
