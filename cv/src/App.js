f9d6471bdec38b77098ec8191563d758b40c771f
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 17:30:19 +0530

    included  education

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
+import Education from './components/education'
+
+import Prismic from 'prismic-javascript'
+import { Date, Link, RichText } from 'prismic-reactjs'
+
 
 import Navigation from './components/navbar';
+import Footer from './components/footer';
+
+
+
+// export async function getServerSideProps() {
+
+//   const getEducation
+// }
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Education/>
+      <Footer/>
     </div>
+
   );
 }
 
