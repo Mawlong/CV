d72a4123ed0ee7bfbde8b5667512ac457c478eee
Leon Patrick Mawlong <leonmawlong@gmail.com>
2020-05-22 18:13:05 +0530

    fixed appearnace

--- cv/src/components/footer.js
+++ cv/src/components/footer.js
@@ -3,50 +3,55 @@ import {Row,Col,Container} from 'react-bootstrap'
 
 function footer() {
     return (
-        <div className="footer text-white" style={{position:"fixed", bottom:"0", width:"100%"}}>
-            <Container className= "mt-5" fluid={true}>
-                <Row className='justify-content-left max-auto py-1' style={{backgroundColor:"#2680d9"}}>
-                    <Col className = 'p-0' md = {3} sm ={12}>
+        <div className="footer text-white bg-dark">
+            
+            <Container className= "mt-2 mb-2" >
+            
+                <Row className="py-1 mx-auto">
+            
+                    <Col className = "p-1" md = {2} sm ={12} align="left">
                         <p>
                             Email:
                         </p>
                     </Col>
 
-                    <Col className = 'p-0' md = {3} sm ={12}>
+                    <Col className = "" md = {2} sm ={12} align="left">
                         <p>
                             <a href="mailto:b16cs004@nitm.ac.in">b16cs004@nitm.ac.in</a>
                         </p>
                     </Col>
                 </Row>
 
-                <Row className='ustify-content-left max-auto py-1' style={{backgroundColor:"#2680d9"}}>
-                    <Col className = 'p-0' md = {3} sm ={12}>
+                <Row className="py-1 mx-auto">
+                    <Col className = "p-1" md = {2} sm ={12} align="left">
                         <p>
                             Phone number:
                         </p>
                     </Col>
 
-                    <Col className = 'p-0' md = {3} sm ={12}>
+                    <Col className = "p-1" md = {2} sm ={12} align="left">
                         <p>
                             <a href="tel=+917308188239">+91 - 7308188239</a>
                         </p>
                     </Col>
                 </Row>
-                <Row className="justify-content-left max-auto py-1" style={{backgroundColor:"#174d82"}}>
-                    <Col className = 'p-0' md = {3} sm ={12}>
-                        <div>
-                            <h5 className="justify-content-left"> Address </h5>
-                            <span>
-                                House no 68, Lower New Colony, Laitumkhrah, Shillong - 793003, Meghalaya, India.
-                            </span>
-                        </div>
+
+                <Row className="py-1 mx-auto">
+                    <Col className = "p-1" md = {2} sm ={12} align="left">
+                        <p>Address</p>
+                    </Col>
+
+                    <Col className="p-1 " md = {6} sm ={12} align="left">
+                        <p>
+                            House no 68, Lower New Colony, Laitumkhrah, Shillong - 793003, Meghalaya, India.
+                        </p>
+                    
                     </Col>
                 </Row>
+
             </Container>
 
-
-        </div>
-    
+        </div>   
             
         
     )
